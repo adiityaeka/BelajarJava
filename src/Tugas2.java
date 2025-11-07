e78ee9522da1b6fdc09b7ae2d453e0b86f6dad43
Aditya Eka <atg.adit.pku@gmail.com>
2025-11-07 07:32:18 +0700

    rename

--- src/Tugas2.java
+++ src/Tugas2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.Locale;
 
 
-public class ifStatement {
+public class Tugas2 {
     public static void main(String[] args){
 
         NumberFormat formatter = NumberFormat.getInstance(new Locale("id", "ID"));
@@ -15,7 +15,7 @@ public class ifStatement {
         char golongan;
         int jamLembur;
         double gajiPokok = 0;
-        double persenLembur = 0;
+        double persenLembur = 0 ;
         double gajiLembur;
         double totalGaji;
 
@@ -56,7 +56,7 @@ public class ifStatement {
         gajiLembur = gajiPokok * persenLembur;
         totalGaji = gajiPokok + gajiLembur;
 
-        System.out.println("Gaji Pokok = " + currency + formatter .format(gajiPokok));
+        System.out.println("\nGaji Pokok = " + currency + formatter .format(gajiPokok));
         System.out.println("Upah Lembur = " + currency + formatter .format(gajiLembur));
         System.out.println("Total Penghasilan : " + currency + formatter .format(totalGaji));
         } else {
